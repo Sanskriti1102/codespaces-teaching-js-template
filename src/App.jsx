059b9b6cb04d556c1d2f5c01b57eb4dcf4ebbe7d
sanskriti <sanskriti.kadam.0211@gmail.com>
2023-10-26 22:14:09 +0530

    committed now

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,44 @@
  */
 
 import React from "react";
+
+import About from "./Components/About";
+import Footer from "./Components/Footer";
 import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Portfolio from "./Components/Portfolio";
+
+import "./styles.css";
+
+/**
+ * This object represents your information. The project is set so that you
+ * only need to update these here, and values are passed a properties to the
+ * components that need that information.
+ *
+ * Update the values below with your information.
+ *
+ * If you don't have one of the social sites listed, leave it as an empty string.
+ */
+const siteProps = {
+  name: "Sanskriti Kadam",
+  title: "Computer Engineer",
+  email: "sanskriti.kadam.0211@gmail.com",
+  gitHub: "https://github.com/Sanskriti1102",
+  linkedIn: "www.linkedin.com/in/sanskritikadam",
+};
+
+const primaryColor = "#163c45";
+const secondaryColor = "#D2F1E4";
 
 const App = () => {
   return (
-    <>
+    <div id="main">
       <Header />
-      <h3>Built using Codespaces! 🥳</h3>
-      <p>Follow instructions in README to utilize this template for your site or class.</p>
-    </>
+      <Home name={siteProps.name} title={siteProps.title} />
+      <About />
+      <Portfolio />
+      <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
+    </div>
   );
 };
 
---
+++ src/Components/Portfolio.jsx
@@ -0,0 +1,80 @@
+/**
+ * Portfolio component
+ *
+ * Highlights some of  your creations. These can be designs, websites,
+ * open source contributions, articles you've written and more.
+ *
+ * This is a great area for you to to continually add to and refine
+ * as you continue to learn and create.
+ */
+
+import React from "react";
+
+/**
+ * Desk image
+ *
+ * Below is a sample desk image. Feel free to update this to an image of your choice,
+ * updating below imageAltText to string that represents what you see in that image.
+ *
+ * Need an image? Check out https://unsplash.com to download a photo you
+ * freely use on your site.
+ */
+import image from "../images/20230306_155643.jpg";
+
+const imageAltText = "desktop with books and laptop";
+
+/**
+ * Project list
+ *
+ * An array of objects that will be used to display for your project
+ * links section. Below is a sample, update to reflect links you'd like to highlight.
+ */
+const projectList = [
+  {
+    title: "Why me?",
+    description:
+      "@Microsoft Certified | COO@CLUMOSS | ALPHA-Microsoft Learn Student Ambassador | @CodingNinjas Entrepreneur | @Techfest IIT Bombay CA| @Zuno CA | SheBuildsOnAzure 2.0,3.0",
+    url: "www.linkedin.com/in/sanskritikadam",
+  },
+  {
+    title: "SK Website",
+    description:
+      "My passion allowing me to explore and ponder over many exciting technologies.",
+    url: "https://linktr.ee/sanskriti_kadam",
+  },
+  {
+    title: "My Resume Site",
+    description:
+      "Created from Microsoft's resume workshop and deployed to Azure. Includes my experience and design abilities.",
+    url: "https://github.com/Sanskriti1102",
+  },
+];
+
+const Portfolio = () => {
+  return (
+    <section className="padding" id="portfolio">
+      <h2 style={{ textAlign: "center" }}>Portfolio</h2>
+      <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
+        <div style={{ maxWidth: "40%", alignSelf: "center" }}>
+          <img
+            src={image}
+            style={{ height: "90%", width: "100%", objectFit: "cover" }}
+            alt={imageAltText}
+          />
+        </div>
+        <div className="container">
+          {projectList.map((project) => (
+            <div className="box" key={project.title}>
+              <a href={project.url} target="_blank" rel="noopener noreferrer">
+                <h3 style={{ flexBasis: "40px" }}>{project.title}</h3>
+              </a>
+              <p className="small">{project.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
